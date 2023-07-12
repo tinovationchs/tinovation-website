f269433a5fde8d589cbcfd2cf242f23cf668ccf9
Daniel Cui <solunian@gmail.com>
2023-07-12 04:44:58 -0700

    add socials bar to home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,21 @@
 <script lang="ts">
-  import HomeLogo from "$lib/components/HomeLogo.svelte";
+  import HomePageLogo from "$lib/components/HomePageLogo.svelte";
+  import SocialsBar from "$lib/components/SocialsBar.svelte";
 </script>
 
 <!-- CONTENT STREAM -->
 <div class="flex w-full flex-col items-center text-retro-black">
-  <div class="flex h-[36rem] w-full flex-row justify-center rounded-b-xl bg-retro-black">
-    <div class="flex flex-col items-center gap-6 pt-56">
-      <HomeLogo />
+  <!-- BANNER/HEADER THINGY -->
+  <div class="flex h-[40rem] w-full flex-row justify-center rounded-b-xl bg-retro-black">
+    <div class="flex flex-col items-center pt-56">
+      <HomePageLogo />
 
-      <h1 class="font-header text-7xl font-semibold text-retro-white">Tinovation</h1>
+      <h1 class="mt-4 font-header text-7xl font-semibold text-retro-white">Tinovation</h1>
+      <h3 class=" font-header text-2xl text-retro-gray">
+        programming club at CHS
+      </h3>
+
+      <SocialsBar />
     </div>
   </div>
 </div>
