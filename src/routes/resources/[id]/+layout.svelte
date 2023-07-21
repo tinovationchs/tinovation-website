0d723ccfe07ea4a29d76cf854b34282daa97d2e2
proud canadian <113315327+downloadablecontent@users.noreply.github.com>
2023-07-20 18:42:52 -0700

    add syntax highlighting
    also add language annotations to code blocks in resource and fix some double quote shenasfsdgans

---
+++ src/routes/resources/[id]/+layout.svelte
@@ -0,0 +1,15 @@
+<svelte:head>
+  <link rel="stylesheet" href="/css/atom-one-dark.min.css">
+  <script src="/js/highlight.min.js"></script>
+</svelte:head>
+
+<slot></slot>
+
+
+<script>
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    hljs?.highlightAll();
+  });
+</script>
