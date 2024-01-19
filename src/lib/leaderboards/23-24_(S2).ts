d47a423f2ecde306ffda21493a5257a49f4c7af4
Daniel Cui <solunian@gmail.com>
2024-01-18 23:03:16 -0800

    chore: update leaderboard S2 | build: npx?

---
+++ src/lib/leaderboards/23-24_(S2).ts
@@ -0,0 +1,91 @@
+import type { Leaderboard } from "$lib/types";
+        
+const data: Leaderboard = [
+  {
+    name: "Ashaaz Ahmed",
+    points: [],
+    comment: "",
+  },
+  {
+    name: "Nikhil Krishnaswamy",
+    points: [],
+    comment: "",
+  },
+  {
+    name: "Jimin Lee",
+    points: [],
+    comment: "",
+  },
+  {
+    name: "Michael Chuang",
+    points: [],
+    comment: "",
+  },
+  {
+    name: "Kavya Gupta",
+    points: [],
+    comment: "",
+  },
+  {
+    name: "Chris Zhang",
+    points: [],
+    comment: "",
+  },
+  {
+    name: "Aaron Dbritto",
+    points: [],
+    comment: "",
+  },
+  {
+    name: "Anshika Jain",
+    points: [],
+    comment: "",
+  },
+  {
+    name: "Shubhi Jain",
+    points: [],
+    comment: "",
+  },
+  {
+    name: "John Kim",
+    points: [],
+    comment: "",
+  },
+  {
+    name: "Pragya Sharma",
+    points: [],
+    comment: "",
+  },
+  {
+    name: "Avi Tomar",
+    points: [],
+    comment: "",
+  },
+  {
+    name: "Jolie Han",
+    points: [],
+    comment: "",
+  },
+  {
+    name: "Alvin Kang",
+    points: [],
+    comment: "",
+  },
+  {
+    name: "Tanay Gupta",
+    points: [],
+    comment: "",
+  },
+  {
+    name: "Dhruva Srinivas",
+    points: [],
+    comment: "",
+  },
+  {
+    name: "Tanay Gupta",
+    points: [],
+    comment: "",
+  },
+];
+
+export default data;
