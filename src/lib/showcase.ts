a1d197fb23696d67caef5ad35ff13d4352cb0514
Daniel Cui <solunian@gmail.com>
2024-04-22 11:13:08 -0700

    chore: add projects to showcase from the entire year

--- src/lib/showcase.ts
+++ src/lib/showcase.ts
@@ -6,20 +6,78 @@ import type { Project } from "./types";
 
 const showcaseProjects: Project[] = [
   {
+    thumbnail_image: "confbooker.png",
+    name: "Conference Room Booker",
+    authors: ["Purva Marfatia"],
+    date: "2023-12-10",
+    description: "...it's a conference room booker :3",
+    main_link:
+      "https://www.loom.com/share/3d350a1781bf45a5aa42ef4ae3694c2b?sid=a7158d81-fb1f-4a04-922f-9a4f40bb0fd8",
+    github_link: "https://github.com/sourcherryscones/tino-conf",
+  },
+  {
+    thumbnail_image: "getalife.png",
+    name: "get-a-life",
+    authors: ["Dhruva Srinivas", "Shine Chang"],
+    date: "2023-12-09",
+    description:
+      "A Chrome extension that aims to improve the user's productivity by blocking websites they waste time on.",
+    main_link: "https://github.com/carrotfarmer/get-a-life",
+    github_link: "https://github.com/carrotfarmer/get-a-life",
+  },
+  {
+    thumbnail_image: "error.jpg",
+    name: "Private TOR Streaming?",
+    authors: ["Taisei De La Cruz"],
+    date: "2023-11-04",
+    description: "Private manga, anime, music viewer on TOR.",
+    main_link: "https://drive.google.com/file/d/1VR1QcVm5w8BUD1Tipd3XMtjy0qxXzfRQ/view?usp=sharing",
+  },
+  {
+    thumbnail_image: "music_transcription.png",
+    name: "Music Transcription",
+    authors: ["Purva Marfatia", "Advaiyt Sane"],
+    date: "2023-11-05",
+    description: "Allows the user to upload an audio file and converts it directly to sheet music!",
+    main_link:
+      "https://www.loom.com/share/18f883ca86e54b459940732d01fc31a9?sid=faefb6f5-37e0-4f23-adbc-3ceb39a56cc1",
+    github_link: "https://github.com/sourcherryscones/music_transcription",
+  },
+  {
+    thumbnail_image: "diamondforge.png",
+    name: "DiamondForge",
+    authors: ["Michael Chuang", "Chris Zhang", "Tanay Gupta"],
+    date: "2023-11-04",
+    description:
+      "A goal-setting app that provides you with ai-generated steps and real-money incentives.",
+    main_link: "https://www.youtube.com/watch?v=VgRh1WlAYBc",
+    github_link: "https://github.com/Chrisandyu/diamondforge",
+  },
+  {
     thumbnail_image: "tinotorial.jpg",
     name: "Tino Tutorial",
-    authors: ["Pragya Sharma, Kavya Gupta, Namita Gaidhani"],
-    date: "2023-10-15",
+    authors: ["Pragya Sharma", "Kavya Gupta", "Namita Gaidhani"],
+    date: "2023-10-07",
     description:
       "This is an organizer for the availability in different rooms during tutorials on Mondays, Wednesdays, and Fridays for Cupertino High School.",
     main_link: "https:/chs-tut-vercel.vercel.app/",
     github_link: "https://github.com/pragisharma/chs-tutorial/",
   },
   {
+    thumbnail_image: "swoosh.png",
+    name: "Project Swoosh",
+    authors: ["Michael Chuang", "Chris Zhang", "Tanay Gupta"],
+    date: "2023-10-07",
+    description:
+      "A web application that automates Spotify media controls with the wave of your hand.",
+    main_link: "https://drive.google.com/file/d/1ihQXmlHnQGJOZvjDwEj4k1T1vdjO6dCP/view?usp=sharing",
+    github_link: "https://github.com/mchuang413/TinovationCodeSprint1",
+  },
+  {
     thumbnail_image: "tinofind.png",
     name: "Tinofind",
     authors: ["Dhruva Srinivas"],
-    date: "2023-10-09",
+    date: "2023-10-06",
     description:
       "Tinofind is a lost and found website for our school, allowing people to report and claim lost items.",
     main_link: "https://tinofind.vercel.app/",
@@ -28,7 +86,7 @@ const showcaseProjects: Project[] = [
   {
     thumbnail_image: "flametest.png",
     name: "VR Chemistry Lab",
-    authors: ["Laurier Ke, Arnav Gupta"],
+    authors: ["Laurier Ke", "Arnav Gupta"],
     date: "2023-09-23",
     description: "A VR application of the flame tests chemistry lab.",
     main_link: "https://drive.google.com/file/d/1OVgo9jz7gu5Zvs1Hv6hH5RGlxUcHnqM7/view?usp=sharing",
