965d4a1b3ef115f8241c8cfce62d72f842ebf42c
Shine Chang <shine00chang@gmail.com>
2024-09-04 22:13:37 -0700

    (fix & update) clipping, first meeting

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -2,10 +2,11 @@
   import { page } from "$app/stores";
 </script>
 
-<div class="fixed flex w-full flex-col items-center gap-4 py-4">
+
+<div class="fixed flex flex-col w-full items-center py-4 gap-4 z-30"> 
   <div>
     <nav
-      class="text-semibold z-30 flex flex-row gap-4 rounded-3xl bg-retro-white px-4 py-1 font-header text-lg opacity-90 shadow-md shadow-transparent transition ease-in hover:-translate-y-0.5 hover:shadow-retro-white md:gap-8 md:px-10 md:py-1.5 md:text-2xl">
+      class="text-semibold flex flex-row gap-4 rounded-3xl bg-retro-white px-4 py-1 font-header text-lg opacity-90 shadow-md shadow-transparent transition ease-in hover:-translate-y-0.5 hover:shadow-retro-white md:gap-8 md:px-10 md:py-1.5 md:text-2xl">
       {#if $page.url.pathname === "/"}
         <a href="/" class="text-amber-700 transition ease-in">home</a>
       {:else}
@@ -31,9 +32,7 @@
       {/if}
     </nav>
   </div>
-  <div class="rounded-3xl bg-retro-white px-4 py-1 font-header text-2xl">
-    Club Faire
-    <a class="text-sky-700 underline" href="https://forms.gle/hvb6C1KAkU781qRX6"
-      >interest forum →</a>
+  <div class="text-2xl font-header bg-retro-white rounded-3xl px-4 py-1">
+    First meeting <span class="text-sky-600">9/16</span>! 307 @ Lunch
   </div>
 </div>
