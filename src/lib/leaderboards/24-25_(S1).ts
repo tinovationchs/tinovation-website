767916c66b2b3e04e2c5db9f86d515bba2fefa84
sourcherryscones <67602230+sourcherryscones@users.noreply.github.com>
2025-01-06 00:39:39 -0800

    updated leaderboard kinda late :')

--- src/lib/leaderboards/24-25_(S1).ts
+++ src/lib/leaderboards/24-25_(S1).ts
@@ -1,5 +1,138 @@
 import type { Leaderboard } from "$lib/types";
 
-const data: Leaderboard = [];
+// gave everyone +3 for boba drops website, lmk if we should change number
+
+const data: Leaderboard = [
+  {
+    name: "Aarush Arya",
+    points: [3,5,10],
+    comment: "",
+  },
+  {
+    name: "Michael Chuang",
+    points: [8,10],
+    comment: "",
+  },
+  {
+    name: "Calvin Chang",
+    points: [3],
+    comment: "",
+  },
+  {
+    name: "Jishnu Ganisetti",
+    points: [3, 10],
+    comment: "",
+  },
+  {
+    name: "Stephen Lin",
+    points: [3, 7],
+    comment: "",
+  },
+  {
+    name: "Aneesh Raghavan",
+    points: [3,5],
+    comment: "",
+  },
+  {
+    name: "Ruiyu Liu",
+    points: [3],
+    comment: "",
+  },
+  {
+    name: "Bernard Freund",
+    points: [3,5],
+    comment: "prussian",
+  },
+  {
+    name: "Richard Zhang",
+    points: [3, 10],
+    comment: "2nd in code sprint #1",
+  },
+  {
+    name: "Aditya Baliyara",
+    points: [3],
+    comment: "",
+  },
+  {
+    name: "Tomoki Takeuchi",
+    points: [3,8],
+    comment: "ML genius",
+  },
+  {
+    name: "Alan Kim",
+    points: [3],
+    comment: "greatest tinovation member since Tanay",
+  },
+  {
+    name: "Jimin Lee",
+    points: [3,5,5],
+    comment: "locker room project!",
+  },
+  {
+    name: "Tejas Bojanapati",
+    points: [3],
+    comment: "",
+  },
+  {
+    name: "Timofey Usubyan",
+    points: [3],
+    comment: "",
+  },
+  {
+    name: "Vismay",
+    points: [3],
+    comment: "",
+  },
+  {
+    name: "Andy Zhang",
+    points: [3],
+    comment: "",
+  },
+  {
+    name: "Dhruva Srinivas",
+    points: [3, 5,5],
+    comment: "",
+  },
+  {
+    name: "Advaiyt Sane",
+    points: [3],
+    comment: "",
+  },
+  {
+    name: "Kavya Gupta",
+    points: [3,7],
+    comment: "Bookstore application - 3rd in code sprint #3",
+  },
+  {
+    name: "Avani Bheemaiah",
+    points: [3],
+    comment: "",
+  },
+  {
+    name: "Ethan Shen",
+    points: [3],
+    comment: "",
+  },
+  {
+    name: "Ayush Bheemaiah",
+    points: [10],
+    comment: "TinoGPT - 1st in code sprint #3",
+  },
+  {
+    name: "Aditya Suresh",
+    points: [10],
+    comment: "TinoGPT - 1st in code sprint #3",
+  },
+  {
+    name: "Chris Zhang",
+    points: [8],
+    comment: "APCS Autograder - 2nd in code sprint #3",
+  },
+  {
+    name: "Shauryan Kanaujia",
+    points: [5],
+    comment: "locker room project!",
+  },
+];
 
 export default data;
