b8917e4ba62e7363e3a9f62ae5c9f9c886f2a1f9
magical paperclip <176186722+magical-paperclip@users.noreply.github.com>
2026-06-17 23:18:33 -0700

    Start website at top on initial load

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,14 @@
 <script>
   import Footer from "$lib/components/Footer.svelte";
   import Navbar from "$lib/components/Navbar.svelte";
+  import { initializeScrollAtTop } from "$lib/scroll.js";
   import "../app.css";
   import { page } from "$app/stores";
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    initializeScrollAtTop(window);
+  });
 </script>
 
 <div class="flex min-h-screen flex-col">
