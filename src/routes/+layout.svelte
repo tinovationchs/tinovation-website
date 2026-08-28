496e3365a6b53618a45e8e0fd2824eb0f6050d6f
magical paperclip <176186722+magical-paperclip@users.noreply.github.com>
2026-08-28 12:23:52 -0700

    feat: implement ThemeSwitcher in layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import Footer from "$lib/components/Footer.svelte";
   import Navbar from "$lib/components/Navbar.svelte";
+  import ThemeSwitcher from "$lib/components/ThemeSwitcher.svelte";
+  import { activeTheme, initTheme } from "$lib/theme";
   import { initializeScrollAtTop } from "$lib/scroll.js";
   import "../app.css";
   import { page } from "$app/stores";
@@ -8,7 +10,18 @@
 
   onMount(() => {
     initializeScrollAtTop(window);
+    initTheme();
   });
+
+  $: if (typeof document !== "undefined" && $activeTheme) {
+    document.documentElement.style.setProperty("--color-retro-black", $activeTheme.colors.black);
+    document.documentElement.style.setProperty("--color-retro-gray", $activeTheme.colors.gray);
+    document.documentElement.style.setProperty(
+      "--color-retro-lightgray",
+      $activeTheme.colors.lightgray
+    );
+    document.documentElement.style.setProperty("--color-retro-white", $activeTheme.colors.white);
+  }
 </script>
 
 <div class="flex min-h-screen flex-col">
@@ -21,4 +34,6 @@
   {#if $page.url.pathname !== "/"}
     <Footer />
   {/if}
+
+  <ThemeSwitcher />
 </div>
