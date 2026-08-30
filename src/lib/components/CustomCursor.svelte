d240867426479334f041f42a5ea131fe9321b41e
magical paperclip <176186722+magical-paperclip@users.noreply.github.com>
2026-08-30 09:02:48 -0700

    style: swap retro cursors for smooth modern macOS Sequoia SVGs

--- src/lib/components/CustomCursor.svelte
+++ src/lib/components/CustomCursor.svelte
@@ -84,27 +84,46 @@
 </script>
 
 {#if isVisible}
-  <!-- Retro Mac Pointer -->
+  <!-- Modern macOS Sequoia Pointer -->
   <div
     class="pointer-events-none fixed left-0 top-0 z-[100] transition-transform duration-75"
     style="
       left: {clientX}px; 
       top: {clientY}px; 
-      transform: translate({isHovering ? '-10.5px' : '0px'}, 0px) scale({isClicking ? 0.85 : 1});
+      transform: translate({isHovering ? '-9px' : '-8px'}, {isHovering
+      ? '-1px'
+      : '-2px'}) scale({isClicking ? 0.85 : 1});
       transform-origin: top left;
+      filter: drop-shadow(0px 2px 4px rgba(0, 0, 0, 0.4));
     ">
     {#if isHovering}
-      <img
-        src="/mac-hand.png"
-        alt="pointing hand"
-        style="image-rendering: pixelated;"
-        class="h-6 w-6 drop-shadow-[2px_2px_0px_rgba(0,0,0,0.5)]" />
+      <svg
+        width="28"
+        height="28"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg">
+        <path
+          d="M10,13.8c-0.2-0.3-0.5-0.8-0.9-1.5c-0.2-0.4-0.9-1.1-1.1-1.4c-0.2-0.3-0.2-0.5-0.1-0.8c0.1-0.5,0.5-0.8,1-0.8c0.4,0,0.8,0.3,1,0.5c0.2,0.2,0.4,0.5,0.5,0.6c0.1,0.2,0.1,0.2,0.3,0.4c0.2,0.2,0.2,0.4,0.1,0.1C10.7,10.5,10.6,9.9,10.5,9.3C10.4,8.9,10.3,8.8,10.2,8.5C10.1,8.1,10.1,7.9,10,7.5C9.9,7.3,9.8,6.7,9.8,6.4C9.7,6,9.7,5.3,10,5C10.2,4.8,10.7,4.7,11,4.9c0.4,0.2,0.6,0.8,0.7,1c0.2,0.4,0.3,0.9,0.4,1.5c0.2,0.8,0.4,1.9,0.4,2.1c0-0.3-0.1-0.8,0-1.1c0.1-0.2,0.2-0.5,0.5-0.6C13.2,7.7,13.5,7.7,13.7,7.7c0.2,0.1,0.5,0.2,0.6,0.4c0.3,0.5,0.3,1.4,0.3,1.4c0.1-0.3,0.1-0.9,0.2-1.2c0.1-0.2,0.4-0.3,0.5-0.4c0.2-0.1,0.5-0.1,0.8,0c0.2,0,0.5,0.2,0.5,0.4C16.8,8.5,16.8,9.3,16.9,9.6c0,0.1,0.1-0.3,0.2-0.5c0.3-0.5,1.4-0.6,1.4,0.5c0,0.5,0,0.5,0,0.8c0,0.4,0,0.6,0,0.9c0,0.3-0.1,1-0.2,1.3c-0.1,0.2-0.3,0.8-0.5,1.1c0,0-0.8,0.9-0.9,1.4c-0.1,0.5-0.1,0.5-0.1,0.8c0,0.3,0.1,0.7,0.1,0.7s-0.6,0.1-0.9,0c-0.3-0.1-0.7-0.6-0.8-0.8c-0.2-0.2-0.4-0.2-0.5,0c-0.2,0.3-0.5,0.8-0.8,0.8c-0.5,0.1-1.6,0-2.3,0c0,0,0.2-0.8-0.2-1.1c-0.2-0.2-0.6-0.6-0.8-0.8L10,13.8z"
+          fill="black"
+          stroke="white"
+          stroke-width="1.2"
+          stroke-linejoin="round" />
+      </svg>
     {:else}
-      <img
-        src="/mac-arrow.png"
-        alt="arrow pointer"
-        style="image-rendering: pixelated;"
-        class="h-6 w-6 drop-shadow-[2px_2px_0px_rgba(0,0,0,0.5)]" />
+      <svg
+        width="24"
+        height="24"
+        viewBox="0 0 32 32"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg">
+        <path
+          d="M10.15 3.19C9.72 2.76 9 3.06 9 3.67V27.39C9 28.02 9.75 28.31 10.17 27.84L14.93 22.56L19.46 31.25C19.64 31.6 20.06 31.75 20.42 31.56L23.47 29.98C23.82 29.79 23.97 29.37 23.8 29.02L19.26 20.4H25.32C25.93 20.4 26.23 19.65 25.79 19.24L10.15 3.19Z"
+          fill="black"
+          stroke="white"
+          stroke-width="1.5"
+          stroke-linejoin="round" />
+      </svg>
     {/if}
   </div>
 {/if}
